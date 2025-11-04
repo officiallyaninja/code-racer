f1079f2d6e6769292f9bb77c60f57bf58cc69395
Arjun Pratap <officiallyaninja@gmail.com>
2025-11-04 15:56:43 +0530

    Update manifest.rs

--- src/manifest.rs
+++ src/manifest.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
-
+use macros::generate_challenge_types;
 use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, path::Path};
 
 //schema version: int
 //time generated: timestamp
@@ -16,6 +16,7 @@ use serde::{Deserialize, Serialize};
 //
 
 #[derive(Serialize, Deserialize, Debug)]
+#[generate_challenge_types]
 pub struct Manifest {
     pub data_version: u32,
     pub conversion: HashMap<String, Conversion>,
@@ -25,6 +26,33 @@ pub struct Manifest {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Conversion {
     pub langs: Vec<String>,
+}
+
+impl Completion {
+    pub fn new_from_fs(path: &Path) -> Self {
+        let mut langs = Vec::new();
+        for lang_folder in path
+            .read_dir()
+            .expect("we got this path fom a dir entry so it should not error")
+        {
+            let lang_folder = lang_folder.unwrap_or_else(|e| {
+                panic!(
+                    "error reading challenges/completion/'{}': {e}",
+                    path.file_name()
+                        .expect("path should have a file_name")
+                        .to_str()
+                        .expect("&Osstr should convert to str")
+                )
+            });
+            langs.push(
+                lang_folder
+                    .file_name()
+                    .into_string()
+                    .expect("failed to convert OSString to String"),
+            );
+        }
+        Self { langs }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
